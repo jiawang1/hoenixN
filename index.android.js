ca881fd42d9f90e6675855933d74c365a9e58186
NieChenghai <chenghai.nie@sap.com>
2016-12-26 11:20:43 +0800

    replace back component

--- index.android.js
+++ index.android.js
@@ -4,36 +4,8 @@ import store from './src/common/configStore';
 import App from './src/containers/App';
 import React from 'react';
 import {AppRegistry, BackAndroid} from 'react-native';
-import * as NavigationStateActions from './src/features/navigation/NavigationState';
 
 const Hoenix = React.createClass({
-
-  componentWillMount() {
-    BackAndroid.addEventListener('hardwareBackPress', this.navigateBack);
-  },
-
-  navigateBack() {
-    const navigationState = store.getState().get('navigationState');
-    const tabs = navigationState.get('tabs');
-    const tabKey = tabs.getIn(['routes', tabs.get('index')]).get('key');
-    const currentTab = navigationState.get(tabKey);
-
-    // if we are in the beginning of our tab stack
-    if (currentTab.get('index') === 0) {
-
-      // if we are not in the first tab, switch tab to the leftmost one
-      if (tabs.get('index') !== 0) {
-        store.dispatch(NavigationStateActions.switchTab(0));
-        return true;
-      }
-      // otherwise let OS handle the back button action
-      return false;
-    }
-
-    store.dispatch(NavigationStateActions.popRoute());
-    return true;
-  },
-
   render() {
     return (
       <Provider store={store}>
